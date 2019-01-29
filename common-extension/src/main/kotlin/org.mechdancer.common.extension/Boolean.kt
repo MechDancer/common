ddd63918efa7c3c59d1ea88df3456e67e0b3f1ce
YangDR <ydrml@hotmail.com>
2019-01-29 15:32:53 +0800

    修改

--- common-extension/src/main/kotlin/org.mechdancer.common.extension/Boolean.kt
+++ common-extension/src/main/kotlin/org.mechdancer.common.extension/Boolean.kt
@@ -8,6 +8,10 @@ fun Boolean.then(block: () -> Unit) = also { if (it) block() }
 /** bool = false 则执行代码块 */
 fun Boolean.otherwise(block: () -> Unit) = also { if (!it) block() }
 
+/** 直接转化到可选的 */
+fun <T> T.toOptional() =
+    Optional<T>(this)
+
 /** 检查一个值 */
 inline fun <T> T.check(block: (T) -> Boolean) =
     if (block(this)) Optional<T>(this) else Optional(Otherwise)
--- common-extension/src/main/kotlin/org.mechdancer.common.extension/Optional.kt
+++ common-extension/src/main/kotlin/org.mechdancer.common.extension/Optional.kt
@@ -1,19 +1,39 @@
 package org.mechdancer.common.extension
 
+/**
+ * 任意的
+ *
+ * 表示一个值可能存在也可能不存在
+ * 作为值的内联类存在，通过一系列函数安全地访问其值
+ */
 @Suppress("NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS")
+@SinceKotlin("1.3")
 inline class Optional<out T>
 @PublishedApi internal constructor(@PublishedApi internal val data: Any?) {
-    object Otherwise
+    @PublishedApi
+    internal object Otherwise
 
+    /** 若值存在则... */
     inline fun then(block: (T) -> Any?) =
         also { if (data !is Otherwise) block(data as T) }
 
+    /** 若值不存在则... */
     inline fun otherwise(block: () -> Any?) =
         also { if (data is Otherwise) block() }
 
+    /** 尝试获取值 */
     fun getOrNull(): T? =
         if (data !is Otherwise) data as T else null
 
+    /** 尝试获取值，不存在则用[else]替换 */
     inline fun <reified U, T : U> getOrElse(`else`: U): U =
         (getOrNull() ?: `else`) as U
+
+    companion object {
+        /** 直接构造一个不存在的值 */
+        val otherwise = Optional<Any?>(Otherwise)
+
+        /** 直接构造一个具有特定类型的不存在的值 */
+        fun <T> otherwise() = Optional<T>(Otherwise)
+    }
 }
