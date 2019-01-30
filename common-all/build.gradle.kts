f09c0b7c8ce8d6fd4abdb04c65ae331cd5812bf1
berberman <1793913507@qq.com>
2019-01-30 13:05:33 +0800

    add jar task

--- common-all/build.gradle.kts
+++ common-all/build.gradle.kts
@@ -14,6 +14,32 @@ task<Jar>("sourcesJar") {
     from(sourceSets["main"].allSource)
 }
 
+tasks.withType<Jar> {
+    for (project in rootProject.subprojects) {
+        from("${project.buildDir}/classes/kotlin/main")
+    }
+}
+
+//task<Jar>("fatJar") {
+//
+//    classifier = "fatJar"
+//    group = "build"
+//
+//    for (project in rootProject.subprojects) {
+//        from("${project.buildDir}/classes/kotlin/main")
+//        from(project.configurations.forEach {
+//            try {
+//                it.map {
+//                    println(it)
+//                    if (it.isDirectory) it else zipTree(it)
+//                }
+//            }catch (e:Exception){
+//                e.printStackTrace()
+//            }
+//        })
+//    }
+//}
+
 configure<PublishExtension> {
     userOrg = "mechdancer"
     groupId = "org.mechdancer"
