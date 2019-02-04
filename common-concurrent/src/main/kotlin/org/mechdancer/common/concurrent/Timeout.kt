175ae8fc5cb5e0a31fb328f598315548086ce558
YdrMaster <ydrml@hotmail.com>
2019-02-04 12:04:35 +0800

    再改个名
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- common-concurrent/src/main/kotlin/org/mechdancer/common/concurrent/Timeout.kt
+++ common-concurrent/src/main/kotlin/org/mechdancer/common/concurrent/Timeout.kt
@@ -3,12 +3,15 @@ package org.mechdancer.common.concurrent
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.async
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 
 /**
  * 反复调用 [block]，直到返回值不为空或超时
  */
-inline fun <T : Any> repeatWithTimeout(timeout: Long, block: () -> T?): T? {
+inline fun <T : Any> repeatWithTimeout(
+    timeout: Long,
+    block: () -> T?
+): T? {
     val start = System.currentTimeMillis()
     while (System.currentTimeMillis() - start < timeout)
         return block() ?: continue
@@ -21,12 +24,11 @@ inline fun <T : Any> repeatWithTimeout(timeout: Long, block: () -> T?): T? {
  * 不会因为在 [block] 内阻塞**线程**而失效，也不产生 [TimeoutCancellationException]，
  * 未能计算出的值用 `null` 表示
  */
-suspend fun <T : Any> runWithTimeout(timeout: Long, block: suspend () -> T): T? {
-    if (timeout <= 0) return null
-    val deferred = GlobalScope.async { block() }
-    return try {
-        withTimeout(timeout) { deferred.await() }
-    } catch (e: TimeoutCancellationException) {
-        null
+suspend inline fun <T : Any> runBlokingWithTimeout(
+    timeout: Long,
+    crossinline block: () -> T
+): T? =
+    timeout.takeIf { it > 0 }?.let {
+        val differed = GlobalScope.async { block() }
+        withTimeoutOrNull(it) { differed.await() }
     }
-}
