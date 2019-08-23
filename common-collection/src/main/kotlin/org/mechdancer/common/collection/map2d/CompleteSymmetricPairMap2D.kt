48bfa6f1f798e6cd4d499f67ed1c901d9c325b29
YangDR <ydrml@hotmail.com>
2019-08-23 11:02:48 +0800

    添加少量注释

--- common-collection/src/main/kotlin/org/mechdancer/common/collection/map2d/CompleteSymmetricPairMap2D.kt
+++ common-collection/src/main/kotlin/org/mechdancer/common/collection/map2d/CompleteSymmetricPairMap2D.kt
@@ -4,29 +4,33 @@ import org.mechdancer.common.extension.then
 
 /**
  * 完全对称表
+ * 添加表头时使用 [map] 构造表项
  */
 class CompleteSymmetricPairMap2D<T, V>(map: (T, T) -> V) :
     ICompleteSymmetricMap2D<T, V> by CompleteSymmetricPairMap2DCore(map) {
 
+    // 二维表的键
     private class Key<T>(private val t0: T, private val t1: T) {
         override fun equals(other: Any?) =
             (other as? Key<*>)?.let {
+                // 对称表，顺序不重要
                 (t0 == it.t0 && t1 == it.t1) || (t0 == it.t1 && t1 == it.t0)
             } == true
 
         override fun hashCode() =
             (t0?.hashCode() ?: 0) + (t1?.hashCode() ?: 0)
 
-        infix fun <V> use(block: (T, T) -> V) = block(t0, t1)
+        infix fun <V> toBuild(block: (T, T) -> V) = block(t0, t1)
 
         fun toPair() = t0 to t1
     }
 
+    // 二维表内核
     private class CompleteSymmetricPairMap2DCore<T, V>
         (override val map: (T, T) -> V) :
-        MutableMap<Key<T>, V> by mutableMapOf(),
+        MutableMap<Key<T>, V> by hashMapOf(),
         ICompleteSymmetricMap2D<T, V> {
-        override val keys0 = mutableSetOf<T>()
+        override val keys0 = hashSetOf<T>()
         override val keys1 get() = keys0
         override fun values0(t: T) = keys0.associateWith { this[t, it] }
         override fun values1(u: T) = values0(u)
@@ -35,7 +39,7 @@ class CompleteSymmetricPairMap2D<T, V>(map: (T, T) -> V) :
 
         override fun put(t: T) {
             keys0.add(t).then {
-                keys0.map { Key(t, it) }.forEach { put(it, it use map) }
+                keys0.map { Key(t, it) }.forEach { put(it, it toBuild map) }
             }
         }
 
